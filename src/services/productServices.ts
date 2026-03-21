5bf030807f1596f6fc14a95219b8c3be56edbcb2
Godwin0397 <godveenpaul@gmail.com>
2026-03-21 17:46:44 +0530

    filter removed after api call

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,8 +1,18 @@
 import { instance } from "./instance";
 
+interface Product {
+    type: string;
+    productName: string;
+    productURL: string;
+  }
+
+type ProductResponse = {
+    allProducts: Product[]
+}
+
 const productServices = {
     allProducts: async () => {
-        return await instance.get('/products')
+        return await instance.get<ProductResponse>('/products')
     }
 }
 
